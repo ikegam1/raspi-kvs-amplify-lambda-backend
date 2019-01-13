4e49138d511a566825d613e483cc9dd2c03d78ac
masrao <takesiba.masrao@gmail.com>
2019-01-13 14:12:33 +0900

    add api

--- app.py
+++ app.py
@@ -19,13 +19,21 @@ streamarn = os.environ.get('KVS_STREAM_ARN')
 cognitopoolarn = os.environ.get('COGNITO_POOL_ARN')
 cognitopoolname = os.environ.get('COGNITO_POOL_NAME')
 streamarn = os.environ.get('KVS_STREAM_ARN')
+iot = boto3.client('iot-data')
+iot_host = os.environ.get('IOT_HOST')
+iot_port = os.environ.get('IOT_PORT')
+iot_topic = os.environ.get('IOT_TOPIC')
+
 authorizer = CognitoUserPoolAuthorizer(
     cognitopoolname, provider_arns=[cognitopoolarn])
 
-@app.route('/', methods=['GET'], content_types=['application/json'], authorizer=authorizer, cors=True)
+@app.route('/', methods=['GET'], authorizer=authorizer, cors=True)
 def get_kvs_endpoint():
-    endpoint = False
-    headers = {"Content-Type" : "application/json", "Access-Control-Allow-Origin": "'*'"}
+    #if path == '/publish_mqtt_servo':
+    #    return publish_mqtt_servo(event,context)
+      
+    endpoint = "False"
+    headers = {"Content-Type" : "application/json", "Access-Control-Allow-Origin": "*", "X-Content-Type-Options": "nosniff"}
     try:
         endpoint = kvs.get_data_endpoint(
             StreamARN=streamarn,
@@ -43,8 +51,9 @@ def get_kvs_endpoint():
         logger.debug(str(url))
         if 'HLSStreamingSessionURL' not in url:
             raise Exception
+        data={'url': url.get("HLSStreamingSessionURL","false")}
         res = {"statusCode" : 200,
-              "body" : url.get("HLSStreamingSessionURL",False),
+              "body" : url.get("HLSStreamingSessionURL","false"),
               "headers" : headers}
         return res
     except ResourceNotFoundException:
@@ -52,10 +61,33 @@ def get_kvs_endpoint():
         endpoint = 'ResourceNotFoundException'
     except:
         logger.debug(traceback.print_exc())
-        pass
 
     res = {"statusCode" : -1,
        "body" : endpoint,
        "headers" : headers}
     return res
 
+@app.route('/mqtt/{angle}', methods=['GET'], authorizer=authorizer, cors=True)
+def publish_mqtt_servo(angle):
+    logger.debug('/mqtt/{angle}')
+    logger.debug(angle)
+
+    headers = {"Content-Type" : "application/json", "Access-Control-Allow-Origin": "*", "X-Content-Type-Options": "nosniff"}
+    try:
+        iot.publish(
+            topic=iot_topic,
+            qos=1,
+            payload="%s" % (angle)
+        )
+        resp = {"statusCode" : 200,
+            "body" : "ok",
+            "headers" : headers
+        }
+        return resp
+    except:
+        logger.debug(traceback.print_exc())
+
+    resp = {"statusCode" : -1,
+       "body" : "error",
+       "headers" : headers}
+    return resp
